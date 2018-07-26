cc2a8b2cc6460f4c6e95e04aa8e8faae9ad6db73
johnkagga <johnkagga@gmail.com>
2018-07-26 09:50:59 +0300

    app starter

--- app/__init__.py
+++ app/__init__.py
@@ -1,36 +1,16 @@
-from flask import Flask, request, jsonify
-from app.utils.actions import Actions
+from flask import Flask
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=["GET"])
 def home():
-    Actions.headlines()
-    return "Home success"
-
-
-@app.route('/lit-bot', methods=['POST'])
-def lit():
-    command = request.form.get('text')
-    slack_user_uid = request.form.get('user_id')
-    response_body = ''
-
-    if command not in Actions.allowed_commands():
-        response_body = {"text": "Invalid command - try `/lit help` for available commands"}
-
-    if command == 'help':
-        response_body = Actions.help()
-
-    if command == 'headlines':
-        response_body = Actions.get_headlines_payload()
-
-    if command == 'my headlines':
-        return Actions.send_headlines_to_user(slack_user_uid)
-
-    if command == 'sports':
-        response_body = Actions.get_sports_payload()
-
-    response = jsonify(response_body)
-    response.status_code = 200
-    return response
+    """
+    Add code to send a message to a channel within a slack workspace
+    1. Create a slack app and get the token
+    2. Create a slack channel and get the slack Id which you then add to
+    the .env file
+    3. Run the app to send the message to the channel
+    :return:
+    """
+    return "Hello Andela Uganda DevCon"
--- app/utils/slack_helper.py
+++ app/utils/slack_helper.py
@@ -1,6 +1,5 @@
 from slackclient import SlackClient
 from config import get_env
-import json
 
 
 class SlackHelper:
@@ -10,15 +9,15 @@ class SlackHelper:
         self.slack_client = SlackClient(self.slack_token)
         self.slack_channel = get_env("SLACK_CHANNEL")
 
-    def post_message(self, msg, recipient):
-        return self.slack_client.api_call(
-            "chat.postMessage",
-            channel=recipient,
-            text=msg,
-            as_user=True
-        )
-
     def post_message_to_channel(self, msg):
+        """
+        This method sends a text message to a slack channel whose Id
+        has been specified.
+        Change the username to match that of your app
+        checkout this https://github.com/slackapi/python-slackclient
+        :param msg:
+        :return:
+        """
         return self.slack_client.api_call(
             "chat.postMessage",
             channel=self.slack_channel,
@@ -26,40 +25,4 @@ class SlackHelper:
             username="lit",
             parse="full",
             as_user=False
-        )
-
-    def post_message_with_attachment(self, message):
-        return self.slack_client.api_call(
-            "chat.postMessage",
-            channel=self.slack_channel,
-            username="lit",
-            parse="full",
-            attachments=json.dumps(message),
-            as_user=False
-        )
-
-    def post_message_to_user_with_attachments(self, message, recipient):
-        return self.slack_client.api_call(
-            "chat.postMessage",
-            channel=recipient,
-            attachments=json.dumps(message),
-            as_user=True
-        )
-
-    def file_upload(self, file_content, file_name, file_type, title=None, ):
-        return self.slack_client.api_call(
-            "files.upload",
-            channels=self.slack_channel,
-            content=file_content,
-            filename=file_name,
-            filetype=file_type,
-            initial_comment='{} Log File'.format(file_name),
-            title=title
-        )
-
-    def get_user_info(self, uid):
-        return self.slack_client.api_call(
-            "users.info",
-            user=uid,
-            token=self.slack_token
         )
